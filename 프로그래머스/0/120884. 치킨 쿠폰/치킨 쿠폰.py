e2cc8c960fe7132bd0d72bbd37404b4ddf4272f7
h2k9971 <108402433+h2k9971@users.noreply.github.com>
2025-02-25 15:15:37 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.py
@@ -0,0 +1,10 @@
+def solution(chicken):
+    answer = 0
+    coupon = chicken
+    
+    while coupon >= 10:
+        new_chicken = coupon // 10
+        answer = answer + new_chicken
+        coupon = new_chicken + (coupon % 10)
+    
+    return answer
